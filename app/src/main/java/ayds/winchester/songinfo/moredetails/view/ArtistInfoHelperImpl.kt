0c7d6657d4dd39d1340780c97db67802272a9048
Cesar Semsey <ce.semsey@gmail.com>
2022-05-11 11:20:59 -0300

    refactor de ArtistInfoHelper, definir constantes, remover !!, dividir logica de artistInfo en más métodos

--- app/src/main/java/ayds/winchester/songinfo/moredetails/view/ArtistInfoHelperImpl.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/view/ArtistInfoHelperImpl.kt
@@ -2,22 +2,42 @@ package ayds.winchester.songinfo.moredetails.view
 
 import java.lang.StringBuilder
 
+const val OPEN_HTML_TAG = "<html><div width=400><font face=\"arial\">"
+const val CLOSE_HTML_TAG = "</font></div></html>"
+const val OPEN_B_TAG = "<b>"
+const val CLOSE_B_TAG = "</b>"
+const val BR_TAG = "<br>"
+const val I_TAG = "(?i)"
+const val OLD_LINE_BREAK = "\n"
+const val SINGLE_QUOTE = "'"
+const val SPACE = " "
+
 interface ArtistInfoHelper {
-    fun artistInfoTextToHtml(info: String, artistName: String?): String
+    fun artistInfoTextToHtml(info: String, artistName: String): String
 }
 
 internal class ArtistInfoHelperImpl : ArtistInfoHelper {
 
-    override fun artistInfoTextToHtml(info: String, artistName: String?): String {
+    private fun addBoldToInfo(info: String, artistName: String): String {
+        return info
+            .replace(SINGLE_QUOTE, SPACE)
+            .replace(OLD_LINE_BREAK, BR_TAG)
+            .replace(
+                I_TAG + artistName.toRegex(),
+                OPEN_B_TAG + artistName.uppercase() + CLOSE_B_TAG
+            )
+    }
+
+    private fun buildHtml(infoHtml: String): String {
         val builder = StringBuilder()
-        builder.append("<html><div width=400>")
-        builder.append("<font face=\"arial\">")
-        val textWithBold = info
-            .replace("'", " ")
-            .replace("\n", "<br>")
-            .replace("(?i)" + artistName!!.toRegex(), "<b>" + artistName.uppercase() + "</b>")
-        builder.append(textWithBold)
-        builder.append("</font></div></html>")
+        builder.append(OPEN_HTML_TAG)
+        builder.append(infoHtml)
+        builder.append(CLOSE_HTML_TAG)
         return builder.toString()
+    }
+
+    override fun artistInfoTextToHtml(info: String, artistName: String): String {
+        val textWithBold = addBoldToInfo(info, artistName)
+        return buildHtml(textWithBold)
     }
 }
