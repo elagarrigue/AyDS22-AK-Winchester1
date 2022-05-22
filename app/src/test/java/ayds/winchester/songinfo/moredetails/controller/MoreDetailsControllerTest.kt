8c8a4f19cbba67774b9096e9b966721b798097ed
Emanuel <somozaemanuel@gmail.com>
2022-05-22 18:24:44 -0300

    Implementados los test para el controller de MoreDetails.

--- app/src/test/java/ayds/winchester/songinfo/moredetails/controller/MoreDetailsControllerTest.kt
+++ app/src/test/java/ayds/winchester/songinfo/moredetails/controller/MoreDetailsControllerTest.kt
@@ -1,4 +1,49 @@
 package ayds.winchester.songinfo.moredetails.controller
 
+import ayds.observer.Subject
+import ayds.winchester.songinfo.moredetails.model.MoreDetailsModel
+import ayds.winchester.songinfo.moredetails.view.MoreDetailsUiEvent
+import ayds.winchester.songinfo.moredetails.view.MoreDetailsUiState
+import ayds.winchester.songinfo.moredetails.view.MoreDetailsView
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
 class MoreDetailsControllerTest {
+
+    private val moreDetailsModel: MoreDetailsModel = mockk(relaxUnitFun = true)
+
+    private val onActionSubject = Subject<MoreDetailsUiEvent>()
+    private val moreDetailsView: MoreDetailsView = mockk(relaxUnitFun = true) {
+        every { uiEventObservable } returns onActionSubject
+    }
+
+    private val moreDetailsController by lazy {
+        MoreDetailsControllerImpl(moreDetailsModel)
+    }
+
+    @Before
+    fun setup() {
+        moreDetailsController.setMoreDetailsView(moreDetailsView)
+    }
+
+    @Test
+    fun `on search event should search artist`() {
+        every { moreDetailsView.uiState } returns MoreDetailsUiState(artistName = "artist")
+
+        onActionSubject.notify(MoreDetailsUiEvent.Search)
+
+        verify { moreDetailsModel.searchArtistInfo("artist") }
+    }
+
+    @Test
+    fun `on view full article event should open external link`() {
+        every { moreDetailsView.uiState } returns MoreDetailsUiState()
+
+        onActionSubject.notify(MoreDetailsUiEvent.ViewFullArticle)
+
+        verify { moreDetailsView.openFullArticle() }
+    }
 }
