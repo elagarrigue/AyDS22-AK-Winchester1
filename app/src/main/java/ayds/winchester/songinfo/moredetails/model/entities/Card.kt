a04b59a0e96b96f97d97c3567017d902fa1102ab
Marco Valla <marcovalla2014@gmail.com>
2022-05-28 17:10:10 -0300

    Se actualizo la clase Card

--- app/src/main/java/ayds/winchester/songinfo/moredetails/model/entities/Card.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/model/entities/Card.kt
@@ -1,8 +1,27 @@
 package ayds.winchester.songinfo.moredetails.model.entities
 
+private const val NO_RESULTS = "No results"
+
 interface Card {
-    val description : String
-    val infoUrl : String
-    val source : String
-    val sourceLogoURL : String
+    val description: String
+    val infoURL: String
+    val source: String
+    val sourceLogoURL: String
+    val isLocallyStored: Boolean
+}
+
+data class CardArtistInfo(
+    override val description: String,
+    override val infoURL: String,
+    override val source: String,
+    override val sourceLogoURL: String,
+    override var isLocallyStored: Boolean = false
+) : Card
+
+object EmptyCard : Card {
+    override val description: String = NO_RESULTS
+    override val infoURL: String = ""
+    override val source: String = ""
+    override val sourceLogoURL: String = ""
+    override var isLocallyStored: Boolean = false
 }
