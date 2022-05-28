a08323c09a2e807baa6e3d842508cc04af251b62
Marco Valla <marcovalla2014@gmail.com>
2022-05-28 16:24:07 -0300

    Se creo la clase Card

---
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/model/entities/Card.kt
@@ -0,0 +1,8 @@
+package ayds.winchester.songinfo.moredetails.model.entities
+
+interface Card {
+    val description : String
+    val infoUrl : String
+    val source : String
+    val sourceLogoURL : String
+}
