8a62106e6d8e2fdd8f68b5b5b154456d36954858
Marianela <madi.0762@gmail.com>
2022-06-05 22:19:51 -0300

    refactor funcion getCard
    agregada funcion mapToCard

--- app/src/main/java/ayds/winchester/songinfo/moredetails/model/repository/external/wikipedia/proxys/ProxyWikipedia.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/model/repository/external/wikipedia/proxys/ProxyWikipedia.kt
@@ -2,6 +2,7 @@ package ayds.winchester.songinfo.moredetails.model.repository.external.wikipedia
 
 import ayds.winchester.songinfo.moredetails.model.entities.Card
 import ayds.winchester.songinfo.moredetails.model.entities.EmptyCard
+import ayds.winchester1.wikipedia.WikipediaArtistInfo
 import ayds.winchester1.wikipedia.WikipediaCardService
 import ayds.winchester1.wikipedia.WikipediaInjector
 
@@ -10,24 +11,24 @@ private const val SOURCE = "Wikipedia"
 internal class ProxyWikipedia () : Proxy {
     private val wikipediaCardService: WikipediaCardService = WikipediaInjector.wikipediaCardService
 
-
     override fun getCard(artist: String?): Card {
-        var card: Card? = null
+        var card: Card?
         val wikipediaCard = wikipediaCardService.getCard(artist)
-        wikipediaCard?.let {
-            card = Card(
-                it.description,
-                it.infoURL,
-                getSource(),
-                it.sourceLogoURL,
-                false
-            )
-        }
-
+        card = wikipediaCard?.mapToCard()
         return card ?: EmptyCard
     }
 
     override fun getSource(): String {
         return SOURCE
+    }
+
+    private fun WikipediaArtistInfo.mapToCard(): Card {
+        return Card(
+            this.description,
+            this.infoURL,
+            getSource(),
+            this.sourceLogoURL,
+            false
+        )
     }
 }
