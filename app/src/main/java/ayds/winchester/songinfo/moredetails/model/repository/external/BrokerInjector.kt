17417b995286e223d48836a8815270f62efffed5
Emanuel <somozaemanuel@gmail.com>
2022-06-07 22:48:41 -0300

    Agregados injectors de los proxies, acomodado el injector del broker e implementado test para ProxyWikipedia a consultar.

--- app/src/main/java/ayds/winchester/songinfo/moredetails/model/repository/external/BrokerInjector.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/model/repository/external/BrokerInjector.kt
@@ -1,18 +1,15 @@
 package ayds.winchester.songinfo.moredetails.model.repository.external
 
-import ayds.winchester1.wikipedia.WikipediaInjector.wikipediaService
-import ayds.lisboa1.lastfm.LastFMInjector.lastFMService
-import ayds.newyork2.newyorkdata.nytimes.NYTimesInjector.nyTimesService
 import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.Proxy
-import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyLastFM
-import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyNewYorkTimes
-import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyWikipedia
+import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyLastFMInjector.proxyLastFM
+import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyNewYorkTimesInjector.proxyNewYorkTimes
+import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyWikipediaInjector.proxyWikipedia
 
 object BrokerInjector {
     private val proxies: List<Proxy> = listOf(
-        ProxyWikipedia(wikipediaService),
-        ProxyLastFM(lastFMService),
-        ProxyNewYorkTimes(nyTimesService)
+        proxyWikipedia,
+        proxyLastFM,
+        proxyNewYorkTimes
     )
     val broker: Broker = BrokerImpl(proxies)
 }
--- app/src/test/java/ayds/winchester/songinfo/moredetails/model/repository/external/proxys/ProxyWikipediaTest.kt
+++ app/src/test/java/ayds/winchester/songinfo/moredetails/model/repository/external/proxys/ProxyWikipediaTest.kt
@@ -1,7 +1,7 @@
 package ayds.winchester.songinfo.moredetails.model.repository.external.proxys
 
 import ayds.winchester.songinfo.moredetails.model.entities.Card
-import ayds.winchester1.wikipedia.WikipediaArtistInfo
+import ayds.winchester.songinfo.moredetails.model.entities.EmptyCard
 import ayds.winchester1.wikipedia.WikipediaService
 import io.mockk.every
 import io.mockk.mockk
@@ -10,14 +10,25 @@ import org.junit.Test
 
 class ProxyWikipediaTest {
 
-    private val wikipediaService: WikipediaService = mockk(relaxUnitFun = true)
-    private val proxyWikipedia: ProxyWikipedia by lazy {
+    private val wikipediaService: WikipediaService = mockk()
+    private val proxyWikipedia: Proxy by lazy {
         ProxyWikipedia(wikipediaService)
     }
 
     @Test
     fun `on found artist name should return artist info card `() {
+        val emptyCard: EmptyCard = mockk(relaxed = true)
+        val infoCard: Card = mockk(relaxed = true)
+        every { proxyWikipedia.getCard("artistName") } returns infoCard
+        Assert.assertNotEquals(emptyCard, infoCard)
+    }
 
+    @Test
+    fun `on not found artist name should return artist info empty card `() {
+        val emptyCard: EmptyCard = mockk(relaxed = true)
+        val infoCard: Card = mockk(relaxed = true)
+        every { proxyWikipedia.getCard("artistName") } returns emptyCard
+        Assert.assertNotEquals(emptyCard, infoCard)
     }
 
 }
