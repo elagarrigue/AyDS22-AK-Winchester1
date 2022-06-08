99c7b4260706178e348497a3c9375c8645a68522
Marco Valla <marcovalla2014@gmail.com>
2022-06-08 15:08:08 -0300

    Se fixearon los test del broker

--- app/src/test/java/ayds/winchester/songinfo/moredetails/model/repository/external/BrokerTest.kt
+++ app/src/test/java/ayds/winchester/songinfo/moredetails/model/repository/external/BrokerTest.kt
@@ -1,6 +1,7 @@
 package ayds.winchester.songinfo.moredetails.model.repository.external
 
 import ayds.winchester.songinfo.moredetails.model.entities.Card
+import ayds.winchester.songinfo.moredetails.model.entities.CardSource
 import ayds.winchester.songinfo.moredetails.model.entities.EmptyCard
 import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyLastFM
 import ayds.winchester.songinfo.moredetails.model.repository.external.proxys.ProxyNewYorkTimes
@@ -18,26 +19,26 @@ class BrokerTest{
     private val broker: Broker = BrokerImpl(listOf(proxyWikipedia,proxyNewYorkTimes,proxyLastFM))
 
     @Test
-    fun `given a  non existing artist, should return a null list`() {
+    fun `given a  non existing artist, should return an empty list`() {
         every {proxyWikipedia.getCard("test")} returns EmptyCard
         every {proxyNewYorkTimes.getCard("test")} returns EmptyCard
         every {proxyLastFM.getCard("test")} returns EmptyCard
 
         val result = broker.getCards("test")
-        val expected : List<Card?> = listOf(null)
+        val expected : List<Card?> = emptyList()
 
         assertEquals(expected,result)
     }
 
     @Test
     fun `given the name of a real artist, should return a card list`(){
-        val cWikipedia : Card("description", "infoUrl", CardSource.WIKIPEDIA, "sourceLogoUrl", false)
-        val cNewYork : Card("description", "infoUrl", CardSource.WIKIPEDIA, "sourceLogoUrl", false)
-        val cLastFm : Card("description", "infoUrl", CardSource.WIKIPEDIA, "sourceLogoUrl", false)
+        val cWikipedia = Card("description", "infoUrl", CardSource.WIKIPEDIA, "sourceLogoUrl", false)
+        val cNewYork = Card("description", "infoUrl", CardSource.NEW_YORK_TIMES, "sourceLogoUrl", false)
+        val cLastFm = Card("description", "infoUrl", CardSource.LAST_FM, "sourceLogoUrl", false)
 
         every {proxyWikipedia.getCard("name")} returns cWikipedia
-        every {proxyNewYorkTimes.getCard("name")} returns cWikipedia
-        every {proxyLastFM.getCard("name")} returns cWikipedia
+        every {proxyNewYorkTimes.getCard("name")} returns cNewYork
+        every {proxyLastFM.getCard("name")} returns cLastFm
 
         val result = broker.getCards("name")
         val expected = listOf(cWikipedia,cNewYork,cLastFm)
